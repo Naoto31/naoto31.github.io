d0b2f13cd59b3b8c120dfea10cd50ac03ac72e6f
Naoto31 <naoto0910sai@gmail.com>
2020-05-28 12:18:40 +0200

    Update

---
+++ index.js
@@ -0,0 +1,12 @@
+// const cardOverlay = document.getElementsByClassName('card-img-overlay')[0];
+// console.log(cardOverlay);
+// const cardBottom = document.getElementsByClassName('card-img')[0];
+
+// cardBottom.addEventListener('mousemove', function(e) {
+//     if (cardOverlay.style.display === 'none') {
+//     cardOverlay.style.display = 'block';
+// } else {
+//     cardOverlay.style.display = 'none';
+// }
+
+// });

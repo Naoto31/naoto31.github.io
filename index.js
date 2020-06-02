db8e26a5820f784649c986a5b0d47ba87c155457
Naoto31 <naoto0910sai@gmail.com>
2020-06-02 21:50:36 +0200

    Update

--- index.js
+++ index.js
@@ -16,4 +16,26 @@ $('.main-carousel').flickity({
     // options
     cellAlign: 'left',
     contain: true
-  });
+});
+
+const flickitySlider = document.getElementsByClassName('flickity-slider')[0];
+console.log(flickitySlider);
+flickitySlider.style.transform = 'translateX(0)';
+
+// const testiDataFlickity = document.getElementsByClassName('testimonial-cards')[0];
+// console.log(testiDataFlickity);
+
+// // function checkPosition(x) {
+// //     if (x.matches) {
+// //         console.log(testiDataFlickity);
+// //         alert('test');    }
+// // }
+
+// // var x = window.matchMedia("(max-width: 768px)");
+// // checkPosition(x);
+
+// $(document).ready(function() {
+//     if($(window).width() <= 768) {
+//         testiDataFlickity.setAttribute('data-flickity', '{"wrapAround": true }');
+//     }
+// }); 

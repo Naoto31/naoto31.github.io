f957d38e25e82a51fd0bfe23acd7a5e9d52e9d72
Naoto31 <naoto0910sai@gmail.com>
2020-06-03 11:15:34 +0200

    Update

--- index.js
+++ index.js
@@ -1,41 +1,70 @@
-// const cardOverlay = document.getElementsByClassName('card-img-overlay')[0];
-// console.log(cardOverlay);
-// const cardBottom = document.getElementsByClassName('card-img')[0];
+// I will be creating a different pen with touch support but right // now I have no time for it due to school
 
-// cardBottom.addEventListener('mousemove', function(e) {
-//     if (cardOverlay.style.display === 'none') {
-//     cardOverlay.style.display = 'block';
-// } else {
-//     cardOverlay.style.display = 'none';
+const slider = document.querySelector(".items");
+const slides = document.querySelectorAll(".item");
+const button = document.querySelectorAll(".button");
+
+let current = 0;
+let prev = 4;
+let next = 1;
+
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
+}
+
+const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
+
+const gotoNum = number => {
+    current = number;
+    prev = current - 1;
+    next = current + 1;
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+        slides[i].classList.remove("prev");
+        slides[i].classList.remove("next");
+    }
+
+    if (next == 5) {
+        next = 0;
+    }
+
+    if (prev == -1) {
+        prev = 4;
+    }
+
+    slides[current].classList.add("active");
+    slides[prev].classList.add("prev");
+    slides[next].classList.add("next");
+}
+
+console.log(slider);
+
+// if ($(window).width() > 1024) {
+//     slider.classList.remove('items');
 // }
 
-// });
+// if ($(window).width() <= 1024) {
+//     slider.classList.add('items');
+// }
 
 
-$('.main-carousel').flickity({
-    // options
-    cellAlign: 'left',
-    contain: true
+jQuery(document).ready(function ($) {
+    var alterClass = function () {
+        var ww = document.body.clientWidth;
+        if (ww > 1024) {
+            $('.items').removeClass('items');
+            $('.testi-card').removeClass('item');
+        } else if (ww <= 1023) {
+            $('.testimonial-cards').addClass('items');
+            $('.testi-card').addClass('item');
+        };
+    };
+    $(window).resize(function () {
+        alterClass();
+    });
+    //Fire it when the page first loads:
+    alterClass();
 });
-
-const flickitySlider = document.getElementsByClassName('flickity-slider')[0];
-console.log(flickitySlider);
-flickitySlider.style.transform = 'translateX(0)';
-
-// const testiDataFlickity = document.getElementsByClassName('testimonial-cards')[0];
-// console.log(testiDataFlickity);
-
-// // function checkPosition(x) {
-// //     if (x.matches) {
-// //         console.log(testiDataFlickity);
-// //         alert('test');    }
-// // }
-
-// // var x = window.matchMedia("(max-width: 768px)");
-// // checkPosition(x);
-
-// $(document).ready(function() {
-//     if($(window).width() <= 768) {
-//         testiDataFlickity.setAttribute('data-flickity', '{"wrapAround": true }');
-//     }
-// }); 
